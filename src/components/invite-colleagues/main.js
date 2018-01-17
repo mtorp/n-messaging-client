76e1a52438e24ff2df7857a7d730e474c16c69ad
Jennifer Kerr <jennifer.kerr@ft.com>
2018-01-17 09:01:25 +0000

    Fix o share (#8)
    * copy new  o-share markup over from existing upsell banner
     🐿 v2.7.0
    
    * updated markup
     🐿 v2.7.0
    
    * synced in latest js and css changes from n-invite-colleagues
     🐿 v2.7.0
    
    * linting
     🐿 v2.7.0
    
    * move z-index
     🐿 v2.7.0
    
    * linting
     🐿 v2.7.0
    
    * linting
     🐿 v2.7.0

--- src/components/invite-colleagues/main.js
+++ src/components/invite-colleagues/main.js
@@ -1,13 +1,18 @@
+const OShare = require('o-share');
 const COPY_BUTTON_SELECTOR = '.invite-colleagues__copy-link-button';
 const COPY_TEXT_SELECTOR = '.invite-colleagues__copy-link';
 const COPY_SUCCESS_CLASS = 'copy-success';
+const NON_INITED_SHARE_CONTAINER_SELECTOR = '[data-o-component=o-share]:not([data-o-share--js])';
 
 module.exports = function customSetup (banner, done) {
-	const copyButtons = banner.innerElement.querySelectorAll(COPY_BUTTON_SELECTOR);
 
-	if (copyButtons) {
-		copyButtons.forEach(button => button.addEventListener('click', () => copyLink(button), false));
+	const shareContainer = banner.innerElement.querySelector(NON_INITED_SHARE_CONTAINER_SELECTOR);
+
+	if (shareContainer) {
+		OShare.init(shareContainer);
 	}
+
+	const copyButtons = banner.innerElement.querySelectorAll(COPY_BUTTON_SELECTOR);
 
 	function copyLink (el) {
 		const copyDiv = el.parentNode;
@@ -21,6 +26,10 @@ module.exports = function customSetup (banner, done) {
 			document.execCommand('copy');
 			copyDiv.classList.add(COPY_SUCCESS_CLASS); // adds after element with tick icon and confirmation text
 		} catch (e) {}
+	}
+
+	if (copyButtons) {
+		copyButtons.forEach(button => button.addEventListener('click', () => copyLink(button), false));
 	}
 
 	done();
