db8b5fa3f9b46c89f0682203df336c6270b07db9
Courtney.Osborn <courtney.osborn@FT-MW4996.local>
2018-02-13 15:25:14 +0000

    Removed js file for ad-blocker-overlay  🐿 v2.7.0

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 const inviteColleagues = require('./invite-colleagues/main');
 const appBanner = require('./desktop-app-banner/main');
 const anonSubscribe = require('./anon-subscribe-now-teal/main');
-const overlay = require('./ad-blocker-overlay/main');
 
 module.exports = {
 	b2bUpsellBanner: inviteColleagues,
 	appPromotingBanner: appBanner,
 	anonSubscribeNow: anonSubscribe,
-	adBlockerOverlay: overlay
 };
