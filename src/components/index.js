0dc0c3109cb2b20000ea34b27df2f31b3929e4e3
Courtney.Osborn <courtney.osborn@FT-MW4996.local>
2018-02-13 07:43:01 +0000

    small variable name change  🐿 v2.7.0

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
 const inviteColleagues = require('./invite-colleagues/main');
 const appBanner = require('./desktop-app-banner/main');
 const anonSubscribe = require('./anon-subscribe-now-teal/main');
-const adBlockerOverlay = require('./ad-blocker-overlay/main');
+const overlay = require('./ad-blocker-overlay/main');
 
 module.exports = {
 	b2bUpsellBanner: inviteColleagues,
 	appPromotingBanner: appBanner,
 	anonSubscribeNow: anonSubscribe,
-	adBlockerOverlay: adBlockerOverlay
+	adBlockerOverlay: overlay
 };
