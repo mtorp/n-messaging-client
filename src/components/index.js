2574f2ba42ea72f516b269c52e942198c2fac74f
Courtney Osborn <courtney.osborn@ft.com>
2018-04-30 14:59:26 +0100

    Removes adBlockerSubBanner and adBlockerRegBanner (#46)
    * Removes adBlockerSubBanner and adBlockerRegBanner
     🐿 v2.8.0
    
    * removes import for ad blocker styling
     🐿 v2.8.0

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,6 @@
 const inviteColleagues = require('./invite-colleagues/main');
 const appBanner = require('./desktop-app-banner/main');
 const anonSubscribe = require('./anon-subscribe-now-teal/main');
-const adBlockerSubBanner = require('./ad-blocker-sub-banner/main');
-const adBlockerRegBanner = require('./ad-blocker-reg-banner/main');
 const navMyFt = require('./nav-myft/main');
 const navContactPreferences = require('./nav-contact-preferences/main');
 
@@ -10,8 +8,6 @@ module.exports = {
 	b2bUpsellBanner: inviteColleagues,
 	appPromotingBanner: appBanner,
 	anonSubscribeNow: anonSubscribe,
-	adBlockerSubBanner,
-	adBlockerRegBanner,
 	navMyFt,
 	navContactPreferences
 };
