ebedad2e904da78d69c55a9685d9f250f81cb53e
D Budge <debugwand@users.noreply.github.com>
2018-05-24 11:21:42 +0100

    Ensure we can set cookies via ajax (#71)
    🐿 v2.8.0

--- src/components/cookie-consent/main.js
+++ src/components/cookie-consent/main.js
@@ -22,7 +22,8 @@ module.exports = function customSetup (banner, done) {
 		event.preventDefault();
 		// call the consent proxy to set other cookies to yes, but still hide banner if those fail. User can manage via preferences pages.
 		return fetch(elemAction, {
-			method: elemMethod
+			method: elemMethod,
+			credentials: 'include'
 		})
 		.then(removeBanner)
 		.catch(error => {
