ceddcc41fd88760bbd919e726d1a7be51ab1cd7c
Courtney Osborn <courtney.lee.osborn@gmail.com>
2018-06-15 13:46:28 +0100

    Removes messageId from manifest  🐿 v2.8.0

--- manifest.js
+++ manifest.js
@@ -1,56 +1,43 @@
 module.exports = {
-	licenceJoinPromptSsiClient: { // name of flag variant
-		path: 'bottom/lazy', // path to template
-		messageId: 'licenceJoinPromptSsiClient' // id as specified in ammit-api
+	licenceJoinPromptSsiClient: { // name of flag variant and must match the messageId in ammit-api
+		path: 'bottom/lazy' // path to template
 	},
 	inviteColleagues: {
-		path: 'bottom/invite-colleagues',
-		messageId: 'inviteColleagues'
+		path: 'bottom/invite-colleagues'
 	},
 	appPromotingBanner: {
-		path: 'bottom/app-promoting-banner',
-		messageId: 'appPromotingBanner'
+		path: 'bottom/app-promoting-banner'
 	},
 	cookieConsentA: {
-		path: 'bottom/cookie-consent',
-		messageId: 'cookieConsentA'
+		path: 'bottom/cookie-consent'
 	},
 	cookieConsentB: {
-		path: 'bottom/cookie-consent',
-		messageId: 'cookieConsentB'
+		path: 'bottom/cookie-consent'
 	},
 	cookieConsentC: {
-		path: 'bottom/cookie-consent',
-		messageId: 'cookieConsentC'
+		path: 'bottom/cookie-consent'
 	},
 	marketingPopupPrompt: {
-		path: 'bottom/lazy',
-		messageId: 'marketingPopupPrompt',
+		path: 'bottom/lazy'
 		guruQueryString: 'offerId=c1773439-53dc-df3d-9acc-20ce2ecde318'
 	},
 	paymentFailure: {
-		path: 'top/payment-failure',
-		messageId: 'paymentFailure'
+		path: 'top/payment-failure'
 	},
 	teamTrial: {
-		path: 'top/team-trial',
-		messageId: 'teamTrial'
+		path: 'top/team-trial'
 	},
 	anonSubscribeNow: {
-		path: 'top/anon-subscribe-now-teal',
-		messageId: 'anonSubscribeNow',
+		path: 'top/anon-subscribe-now-teal'
 	},
 	navAccountSettings: {
 		path: 'top/nav-account-settings',
-		messageId: 'navAccountSettings',
 		tooltip: true
 	},
 	tlsDeprecation: {
-		path: 'top/tls-deprecation',
-		messageId: 'tlsDeprecation'
+		path: 'top/tls-deprecation'
 	},
 	deloitteSubscription: {
-		path: 'top/deloitte-subscription',
-		messageId: 'deloitteSubscription'
+		path: 'top/deloitte-subscription'
 	}
 };
