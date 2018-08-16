37adbee45f6047503213875d6518089d0e2653e1
Liam Keaton <liamkeaton@gmail.com>
2018-08-16 13:15:00 +0100

    Removing old secret (#109)
    🐿 v2.9.0

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -8,7 +8,6 @@ module.exports = {
 		denyOverrides: [
 			'45e2193a-f479-11e7-8c80-69ca88a9e8d1', // README.md:204
 			'c1773439-53dc-df3d-9acc-20ce2ecde318', // manifest.js:24
-			'c1b046a6-4b46-dc66-9bed-9f77389b619a', // templates/partials/bottom/usa-sale.html:25
 			'27e66825-bc4f-ba59-a3cd-1a8f08579bfe', // templates/partials/top/anon-subscribe-now-teal.html:3
 			'1c06e610-c7a7-c0fe-3356-e498223688ae' // templates/partials/top/usa-sale.html:3
 		]
