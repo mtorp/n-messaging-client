e652648208b2b7d63c26f6431adbbdf4f567b2f8
Jennifer Johnson <jennifer.kerr@ft.com>
2018-09-18 14:50:31 +0100

    dynamically generate list of messages for testing (#117)
     🐿 v2.10.3
    
    Whilst yak-shaving on another issue on this repo yesterday i noticed the list of features for pa11y testing was hardcoded. As this list changes pretty frequently and the tests are somewhat tucked away (i.e. easy to forget to update) I thought it would be better to generate the list dynamically from the manifest which _should_ be representative of reality

--- test/smoke.js
+++ test/smoke.js
@@ -1,39 +1,22 @@
-const messages = [
-	{
-		name: 'b2bUpsellBanner',
-		slot: 'messageSlotBottom'
-	},
-	{
-		name: 'appPromotingBanner',
-		slot: 'messageSlotBottom'
-	},
-	{
-		name: 'cookieConsent',
-		slot: 'messageSlotBottom'
-	},
-	{
-		name: 'paymentFailure',
-		slot: 'messageSlotTop'
-	},
-	{
-		name: 'anonSubscribeNow',
-		slot: 'messageSlotTop'
-	},
-	{
-		name: 'swgEntitlementsPrompt',
-		slot: 'messageSlotBottom'
-	}
-];
+const manifest = require('../manifest');
 
-const messageSlotTests = messages.map(message => {
+const parseSlot = (path) => {
+	if (path.includes('bottom')) return 'messageSlotBottom';
+	if (path.includes('top')) return 'messageSlotTop';
+};
+
+// create array for pa11y, with url and FT flag setting for each message
+const createTest = (key) => {
+	const slotName = parseSlot(manifest[key].path);
 	return {
 		headers: {
-			'FT-Flags': `${message.slot}:${message.name}`
+			'FT-Flags': `${slotName}:${key}`
 		},
 		urls: {
-			[`/${message.slot}-${message.name}`]: 200 // url is irrelevant mainly to help debug
+			[`/${slotName}-${key}`]: 200 // url is irrelevant mainly to help debug
 		}
 	};
-});
+};
+const messageSlotTests = Object.keys(manifest).map(createTest);
 
 module.exports = messageSlotTests;
