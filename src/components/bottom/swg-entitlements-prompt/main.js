b669e3f0c8f71655c5d905c239099e526388d4e7
Liam Keaton <liamkeaton@gmail.com>
2018-10-22 09:23:36 +0100

    Bump to v1 and use google dialogs (#121)
    🐿 v2.9.0

--- src/components/bottom/swg-entitlements-prompt/main.js
+++ src/components/bottom/swg-entitlements-prompt/main.js
@@ -1,24 +1,11 @@
 const swgLoader = require('n-swg').swgLoader;
 
 module.exports = (banner, done) => {
-		let swg;
-		return swgLoader({ customOnwardJourney: true })
-			.then(loaded => {
-				swg = loaded;
+		return swgLoader()
+			.then(swg => {
 				swg.init();
 				return swg.checkEntitlements();
-			}).then(entitlements => {
-				if (entitlements && entitlements.hasEntitlements) {
-					// get cta properties from n-swg and apply them to the banner button
-					const ctaProperties = swg.getEntitledOnwardJourneyProps(entitlements);
-					const bannerCta = banner.bannerElement.querySelector('.n-messaging-banner__button');
-					bannerCta.innerHTML = ctaProperties.copy;
-					bannerCta.setAttribute('href', ctaProperties.href);
-					bannerCta.addEventListener('click', ctaProperties.callback);
-
-					done();
-				} else {
-					done({skip: true});
-				}
-			});
+			})
+			.catch(() => { /* fail silently */ })
+			.finally(done);
 };
