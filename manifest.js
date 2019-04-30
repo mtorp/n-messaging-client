fe70789d51731384aec54635eac696a38e4196a0
Kate Beard <asouthernbellein@gmail.com>
2019-04-30 16:01:36 +0100

    Testing linting  🐿 v2.12.3

--- manifest.js
+++ manifest.js
@@ -1,67 +1,70 @@
 module.exports = {
-	licenceJoinPromptSsiClient: { // name of flag variant and must match the messageId in ammit-api
-		path: 'bottom/lazy', // path to template
-		lazy: true
-	},
-	inviteColleagues: {
-		path: 'bottom/invite-colleagues'
-	},
-	appPromotingBanner: {
-		path: 'bottom/app-promoting-banner'
-	},
-	cookieConsentA: {
-		path: 'bottom/cookie-consent'
-	},
-	cookieConsentB: {
-		path: 'bottom/cookie-consent'
-	},
-	cookieConsentC: {
-		path: 'bottom/cookie-consent'
-	},
-	swgEntitlementsPrompt: {
-		path: 'bottom/swg-entitlements-prompt'
-	},
-	marketingPopupPrompt: {
-		path: 'bottom/lazy',
-		lazy: true,
-		guruQueryString: 'offerId=c1773439-53dc-df3d-9acc-20ce2ecde318'
-	},
-	paymentFailure: {
-		path: 'top/payment-failure'
-	},
-	teamTrial: {
-		path: 'bottom/team-trial'
-	},
-	anonSubscribeNow: {
-		path: 'top/anon-subscribe-now'
-	},
-	navAccountSettings: {
-		path: 'top/nav-account-settings',
-		tooltip: true
-	},
-	onboardingMyFt: {
-		path: 'bottom/onboarding-myft'
-	},
-	onboardingPremium: {
-		path: 'bottom/onboarding-premium'
-	},
-	onboardingApp: {
-		path: 'bottom/onboarding-app'
-	},
-	onboardingRegion: {
-		path: 'top/onboarding-region',
-		tooltip: true
-	},
-	bookYourConsult: {
-		path: 'bottom/book-your-consult'
-	},
-	brexitCampaign: {
-		path: 'bottom/brexit'
-	},
-	techScrollAsia: {
-		path: 'bottom/tech-scroll-asia'
-	},
-	appPromoMobile: {
-		path: 'bottom/app-promo-mobile'
-	},
+  licenceJoinPromptSsiClient: { // name of flag variant and must match the messageId in ammit-api
+    path: 'bottom/lazy', // path to template
+    lazy: true
+  },
+  inviteColleagues: {
+    path: 'bottom/invite-colleagues'
+  },
+  appPromotingBanner: {
+    path: 'bottom/app-promoting-banner'
+  },
+  cookieConsentA: {
+    path: 'bottom/cookie-consent'
+  },
+  cookieConsentB: {
+    path: 'bottom/cookie-consent'
+  },
+  cookieConsentC: {
+    path: 'bottom/cookie-consent'
+  },
+  swgEntitlementsPrompt: {
+    path: 'bottom/swg-entitlements-prompt'
+  },
+  marketingPopupPrompt: {
+    path: 'bottom/lazy',
+    lazy: true,
+    guruQueryString: 'offerId=c1773439-53dc-df3d-9acc-20ce2ecde318'
+  },
+  paymentFailure: {
+    path: 'top/payment-failure'
+  },
+  teamTrial: {
+    path: 'bottom/team-trial'
+  },
+  anonSubscribeNow: {
+    path: 'top/anon-subscribe-now'
+  },
+  navAccountSettings: {
+    path: 'top/nav-account-settings',
+    tooltip: true
+  },
+  onboardingMyFt: {
+    path: 'bottom/onboarding-myft'
+  },
+  onboardingPremium: {
+    path: 'bottom/onboarding-premium'
+  },
+  onboardingApp: {
+    path: 'bottom/onboarding-app'
+  },
+  onboardingRegion: {
+    path: 'top/onboarding-region',
+    tooltip: true
+  },
+  bookYourConsult: {
+    path: 'bottom/book-your-consult'
+  },
+  brexitCampaign: {
+    path: 'bottom/brexit'
+  },
+  techScrollAsia: {
+    path: 'bottom/tech-scroll-asia'
+  },
+  techScrollAsia2: {
+    path: 'bottom/tech-scroll-asia'
+  },              
+  appPromoMobile: {
+    path: 'bottom/app-promo-mobile'
+  },
 };
