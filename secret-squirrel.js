4a6b362064c8b5abb9eb710b333941322ec2a5bd
Liam Keaton <liamkeaton@gmail.com>
2019-10-10 10:57:13 +0100

    Add segment Ids to the URLs in the new adgenda  🐿 v2.12.3

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -6,14 +6,15 @@ module.exports = {
 	strings: {
 		deny: [],
 		denyOverrides: [
-			'45e2193a-f479-11e7-8c80-69ca88a9e8d1', // README.md:188
+			'45e2193a-f479-11e7-8c80-69ca88a9e8d1', // README.md:187
 			'4312a4ca-aeac-11e8-99ca-68cf89602132', // demos/templates/layouts/custom-vanilla.html:14
 			'97212eb0-abb9-11e8-8253-48106866cd8a', // demos/templates/layouts/custom-vanilla.html:29
-			'c1773439-53dc-df3d-9acc-20ce2ecde318', // manifest.js:27
+			'c1773439-53dc-df3d-9acc-20ce2ecde318', // manifest.js:21
 			'ed76f4a7-dcc0-f60d-5f92-0bd9b43f30dd', // templates/partials/bottom/b2b-trial-contact-us.html:21
-			'e8ab77e7-19fc-20af-e9c2-7afe11ca167c', // templates/partials/bottom/content-message.html:15
+			'eb62aef2-4bdc-0f31-a2c5-6532b8e17896', // templates/partials/bottom/content-message.html:15
+			'ae5ae5b5-cf1e-6ecc-d83e-4af44b410722', // templates/partials/bottom/new-agenda.html:16
 			'5f60b8b4-cbf0-18d7-df41-9caa1171e8c1', // templates/partials/top/anon-subscribe-now.html:5|18
-			'eb62aef2-4bdc-0f31-a2c5-6532b8e17896'	// templates/partials/bottom/content-message.html:15
+			'58faf1a5-41a3-0485-e2b0-f045491b585a' // templates/partials/top/new-agenda.html:4
 		]
 	}
 };
