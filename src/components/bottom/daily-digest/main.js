34258a67d61bd0b915d5e8d77382fed6da7a8e54
Ben Fletcher <ben.fletcher@ft.com>
2020-01-20 17:41:45 +0000

    whitespace fixes 🐿 v2.12.5

--- src/components/bottom/daily-digest/main.js
+++ src/components/bottom/daily-digest/main.js
@@ -1,12 +1,12 @@
 import signUp from './sign-up';
 
-module.exports = function customSetup(banner, done) {
+module.exports = function customSetup (banner, done) {
 	const button = banner.innerElement.querySelector('[data-n-messaging-action]');
 
-	function handleSignUpClick(evt) {
+	function handleSignUpClick (evt) {
 		evt.preventDefault();
 		// FIXME replace the hardcoded test arguments
-		signUp('World', 'http://www.ft.com/ontology/Topic', '82645c31-4426-4ef5-99c9-9df6e0940c00')
+		signUp('World', 'http://www.ft.com/ontology/Topic', '82645c31-4426-4ef5-99c9-9df6e0940c00');
 		return false;
 	};
 
--- src/components/bottom/daily-digest/sign-up.js
+++ src/components/bottom/daily-digest/sign-up.js
@@ -1,34 +1,34 @@
 import myFtClient from 'next-myft-client';
 import getToken from './get-token';
 
-export default function signUp(conceptTitle, directType, conceptId) {
-  // reused & simplified code from myFT's digest promo
-  function addToDigest() {
-          const csrfToken = getToken();
-          const metaConcept = {
-                  name: conceptTitle
-          };
+export default function signUp (conceptTitle, directType, conceptId) {
+	// reused & simplified code from myFT's digest promo
+	function addToDigest () {
+		const csrfToken = getToken();
+		const metaConcept = {
+			name: conceptTitle
+		};
 
-          if (directType) {
-                  metaConcept.directType = directType;
-          }
+		if (directType) {
+			metaConcept.directType = directType;
+		}
 
-          const metaEmail = {
-                  _rel: {
-                          type: 'daily',
-                          timezone: 'Europe/London'
-                  }
-          };
+		const metaEmail = {
+			_rel: {
+				type: 'daily',
+				timezone: 'Europe/London'
+			}
+		};
 
-          const promises = [myFtClient.add('user', null, 'preferred', 'preference', 'email-digest', Object.assign({}, {token: csrfToken}, metaEmail))];
+		const promises = [myFtClient.add('user', null, 'preferred', 'preference', 'email-digest', Object.assign({}, {token: csrfToken}, metaEmail))];
 
-          if (conceptId) {
-                  promises.push(myFtClient.add('user', null, 'followed', 'concept', conceptId, Object.assign({}, {token: csrfToken}, metaConcept)));
-          }
+		if (conceptId) {
+			promises.push(myFtClient.add('user', null, 'followed', 'concept', conceptId, Object.assign({}, {token: csrfToken}, metaConcept)));
+		}
 
-          return Promise.all(promises);
-  }
+		return Promise.all(promises);
+	}
 
-  myFtClient.init()
-    .then(addToDigest);
+	myFtClient.init()
+		.then(addToDigest);
 }
