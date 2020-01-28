bc9507fecf33b3acb1320b30530374f7cc0350e1
sentayhu.mekoonnali <sentayhu.mekoonnali@FT-MW4737.local>
2020-01-28 16:50:24 +0000

    Remove html creation using javascript and twick it with scss 🐿 v2.12.5

--- src/components/bottom/daily-digest/main.js
+++ src/components/bottom/daily-digest/main.js
@@ -3,22 +3,12 @@ import myft from 'next-myft-client';
 module.exports = function customSetup (banner, done) {
 	if (window.FT && window.FT.flags && window.FT.flags.oneClickDailyDigest) {
 		function generateSuccessHtmlContent () {
-			const parent1Elements = banner.bannerElement.querySelector('.o-banner__content');
-			const childElement1 = document.createElement('div');
-			childElement1.innerHTML = '&#10004;<strong>&ensp;&ensp;Great! You\'re all set for the daily Email Digest.</strong> Get even more FT content deliverd straight to your inbox';
-			childElement1.style.color = '#00572C';
-			childElement1.style.fontSize = '16px';
-			childElement1.style.margin = '16px 24px 16px 0px';
-			parent1Elements.parentNode.replaceChild(childElement1, parent1Elements);
-
-			const parent2Elements = banner.bannerElement.querySelector('.o-banner__actions');
-			const childElement2 = document.createElement('div');
-			childElement2.classList.add('o-banner__action');
-			childElement2.innerHTML = '<a href="https://www.ft.com/newsletters" target="_blank" class="o-banner__link-browse-news-letter"> Browse all newsletters </a>';
-			parent2Elements.parentNode.replaceChild(childElement2, parent2Elements);
-
-			const grandParentElement = banner.bannerElement;
-			grandParentElement.querySelector('.o-banner__outer').style.background='#CCDFC7';
+			const outerContainer = banner.bannerElement.querySelector('.o-banner__outer');
+			outerContainer.classList.add('o-banner_success-background');
+			const signupContent = banner.bannerElement.querySelector('.signup-content');
+			signupContent.classList.add('--is-visible');
+			const successContent = banner.bannerElement.querySelector('.success-content');
+			successContent.classList.remove('--is-visible');
 		}
 
 		function handleSignUpClick (evt) {
@@ -33,7 +23,7 @@ module.exports = function customSetup (banner, done) {
 					console.log({ info: 'could not add user to concept', conceptId, err });
 				}
 				myft.init().then(addUserToConcept).catch(logError);
-				generateSuccessHtmlContent(); // This may not be the place where to call the confirmation message
+				//enerateSuccessHtmlContent(); // This may not be the place where to call the confirmation message
 			}
 			// FIXME remove this debugging line after test
 			/* eslint no-console:0 */
