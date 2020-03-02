2f67d72be55f3bb947fb34e9c57cf1fdbe9e608a
Ben Fletcher <ben.fletcher@ft.com>
2020-03-02 16:21:49 +0000

    Use o-message for colour styling, which wasn't as straightforward as we'd have liked. Clearer separation of concerns between JS and CSS coding. 🐿 v2.12.5

--- client/components/bottom/oneclick-daily-digest/main.js
+++ client/components/bottom/oneclick-daily-digest/main.js
@@ -1,33 +1,33 @@
 import signup from './signup';
-export default (banner, done) => {
-	//if (window.FT && window.FT.flags && window.FT.flags.oneClickDailyDigest) {
-	if(true) {
-		function loadSuccessContent () {
-			const outerContainer = banner.bannerElement.querySelector('.o-banner__outer');
-			outerContainer.classList.add('o-banner_success-background');
-			const closeButton = banner.bannerElement.querySelector('.o-banner__close');
-			closeButton.classList.add('o-banner__close-color');
-			const signupContent = banner.bannerElement.querySelector('.o-banner_signup-content');
-			signupContent.classList.add('o-banner_is-visible');
-			const successContent = banner.bannerElement.querySelector('.o-banner_success-content');
-			successContent.classList.remove('o-banner_is-visible');
-		}
 
-		function handleSignUpClick (evt) {
-			evt.preventDefault();
-			function logError (err) {
-				/* eslint no-console:0 */
-				console.log({ info: 'could not add user to the daily digest', err });
-			}
-
-			signup().then(loadSuccessContent).catch(logError);
-			return false;
-		}
-
-		const link = banner.innerElement.querySelector('[data-n-messaging-action]');
-		link.addEventListener('click', handleSignUpClick);
+export default function (banner, done) {
+	function loadSuccessContent () {
+		// hide front banner
+		banner.bannerElement.querySelector('[data-n-messaging-daily-digest-front]').classList.add('n-messaging-daily-digest--hidden');
+		// move the close button to the back banner
+		banner.closeButtonElement.parentNode.removeChild(banner.closeButtonElement);
+		banner.bannerElement.querySelector('[data-daily-digest-actions]').appendChild(banner.closeButtonElement);
+		// make the banner green (success)
+		banner.bannerElement.classList.add('o-banner--success');
+		// show the back banner
+		banner.bannerElement.querySelector('[data-n-messaging-daily-digest-back]').classList.remove('n-messaging-daily-digest--hidden');
 	}
 
-	done();
+	function logError (err) {
+		/* eslint no-console:0 */
+		console.log({ info: 'could not add user to the daily digest', err });
+	}
 
-};
+	function handleSignUpClick (evt) {
+		evt.preventDefault();
+		signup()
+			.then(loadSuccessContent)
+			.catch(logError);
+		return false;
+	}
+
+	const signUpButton = banner.bannerElement.querySelector('[data-daily-digest-sign-up]');
+	signUpButton.addEventListener('click', handleSignUpClick);
+
+	done();
+}
--- client/components/bottom/oneclick-daily-digest/signup.js
+++ client/components/bottom/oneclick-daily-digest/signup.js
@@ -1,17 +1,18 @@
 import { client as myft } from 'n-myft-ui/myft';
 import getCsrfToken from 'n-myft-ui/myft/ui/lib/get-csrf-token';
-const csrfToken = getCsrfToken();
 
-const addUserToDigest = () => {
-	return myft.add('user', null, 'preferred', 'preference', 'email-digest', { csrfToken,
+export default function signup () {
+	return myft.init()
+		.then(addUserToDigest);
+}
+
+function addUserToDigest () {
+	const csrfToken = getCsrfToken();
+	return myft.add('user', null, 'preferred', 'preference', 'email-digest', {
+		token: csrfToken,
 		_rel: {
 			type: 'daily',
 			sendTime:'every morning'
 		}
 	});
-};
-
-export default () => {
-	myft.init();
-	return addUserToDigest().then(true);
-};
+}
